30ac0ca83cef66144ae26ac5c0ba6819afa0c2ea
Jamie Mari Ciron <jociron@up.edu.ph>
2023-05-21 09:38:00 +0800

    feat: added about page

--- src/application/pages/About.java
+++ src/application/pages/About.java
@@ -17,21 +17,21 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-public class Instructions {
+public class About {
+
     // PROPERTIES:
 	private Scene scene;
 	private Stage stage;
 	private ImageView imgView;
 	private Text title;
 	private Text body;
-	private Text controls;
 	private VBox vbox;
 
 	// CONSTANTS:
 	public static final int WINDOW_HEIGHT = 408;
 	public static final int WINDOW_WIDTH = 768;
 
-	public Instructions() {
+	public About() {
 		StackPane root = new StackPane();
 		this.scene = new Scene(root, Menu.WINDOW_WIDTH, Menu.WINDOW_HEIGHT);
 		this.imgView = this.createBG();
@@ -50,7 +50,7 @@ public class Instructions {
 
     private VBox createVBox() {
     	VBox vbox = new VBox();
-        this.title = new Text("Instructions");
+        this.title = new Text("About");
         this.title.setX(0);
         this.title.setY(0);
         this.title.setFill(Color.web("#FFD7BD",1.0)); 
@@ -59,7 +59,7 @@ public class Instructions {
 		this.title.setStroke(Color.web("#614635",1.0));
 		this.title.setStrokeWidth(2);
 
-        this.body = new Text("A player has a base (castle) and a cannon where it can\n attack the invaders. Killing an invader earns the player\n a point. When the invader reaches the base, the castle\n loses 5 health points. The castle with the highest points after\n 60 seconds, provided that the castle is still alive,\n is declared the winner.");
+        this.body = new Text("Group 3 - CMSC 173 AB-7L \nAndreau Aranton \nJamie Mari Ciron \nApraem Cayle Mabaga");
         this.body.setX(0);
         this.body.setY(0);
         this.body.setFill(Color.web("#FFFFFF",1.0)); 
@@ -68,16 +68,6 @@ public class Instructions {
 		this.body.setStroke(Color.web("#31484B",1.0));
 		this.body.setStrokeWidth(1);
         this.body.setTextAlignment(TextAlignment.CENTER);
-
-        this.controls = new Text("Move using LEFT and RIGHT keys.\n Shoot using the SPACEBAR.");
-        this.controls.setX(0);
-        this.controls.setY(0);
-        this.controls.setFill(Color.web("#43839D",1.0)); 
-		Font controlsFont = Font.font("Impact",FontWeight.EXTRA_BOLD,20);
-		this.controls.setFont(controlsFont);
-		this.controls.setStroke(Color.web("#FFFFFF",1.0));
-		this.controls.setStrokeWidth(1);
-        this.controls.setTextAlignment(TextAlignment.CENTER);
 
         vbox.setAlignment(Pos.CENTER);
         vbox.setPadding(new Insets(7));
@@ -89,7 +79,7 @@ public class Instructions {
         b1.setStyle("-fx-font: 15 Verdana; -fx-background-color: #369DC6; -fx-text-fill: #FFFAFA; "
         		+ "-fx-border-color:#FFFAFA; -fx-border-width: 3px; -fx-border-radius: 20; -fx-background-radius: 20;");
 
-        vbox.getChildren().addAll(title, body, controls);
+        vbox.getChildren().addAll(title, body);
         vbox.getChildren().add(b1);
         
 		this.startMenu(b1);
