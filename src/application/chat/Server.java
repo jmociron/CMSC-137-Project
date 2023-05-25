d1dfc887f462e73f280d77fa722991f3999f3f39
Andreau Aranton <aoaranton@up.edu.ph>
2023-05-25 23:42:09 +0800

    add: ingame chat

--- src/application/chat/Server.java
+++ src/application/chat/Server.java
@@ -38,7 +38,7 @@ public class Server {
     }
 
     public static void main(String[] args) throws IOException{
-        ServerSocket serverSocket = new ServerSocket(1236);
+        ServerSocket serverSocket = new ServerSocket(4000);
         Server server = new Server(serverSocket);
         server.startServer();
 
